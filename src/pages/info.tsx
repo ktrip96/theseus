c155b656026d4d7988863891dfe7299d5ff7d8ee
ktrip96 <ktrip96@gmail.com>
2023-07-02 09:56:32 +0300

    Information Page

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,13 +1,45 @@
 /* eslint-disable react/no-unescaped-entities */
 
+import ThesisInfo from '@/components/ThesisInfo'
+import ThesisRule from '@/components/ThesisRule'
+import { TypographyH4 } from '@/components/typographies/TypographyH4'
+import { TypographyP } from '@/components/typographies/TypographyP'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import React from 'react'
 
-type Props = {}
-
-const info = (props: Props) => {
+const info = () => {
 	return (
-		<div>
-			<div className='w-3/4 m-auto my-4'>
+		<div className='container py-4'>
+			<TypographyH4>Διπλωματική Εργασία</TypographyH4>
+			<TypographyP>
+				Η Διπλωματική Εργασία (Δ.Ε.) είναι μία εκτεταμένη αναλυτική ή συνθετική εργασία που εκπονείται από τους
+				τελειόφοιτους φοιτητές στο τέλος των σπουδών τους με σκοπό την ολοκλήρωση των γνώσεών τους και την
+				παρουσίαση των ικανοτήτων τους στην επεξεργασία αυτοτελών θεμάτων της Επιστήμης του Ηλεκτρολόγου
+				Μηχανικού και Μηχανικού Υπολογιστών. Επισημαίνεται η σημασία της διπλωματικής εργασίας τόσο ως
+				κορύφωσης της πολύχρονης προσπάθειας κάθε σπουδαστή όσο και ως τελευταίου σταδίου για τη δημιουργία
+				ενός Μηχανικού και επιστήμονα και την ενσωμάτωσή του στο χώρο της εργασίας και της κοινωνίας
+				γενικότερα.
+			</TypographyP>
+
+			<Tabs defaultValue='rule'>
+				<TabsList className='grid w-full grid-cols-2 mt-4'>
+					<TabsTrigger value='rule'>Κανονισμός</TabsTrigger>
+					<TabsTrigger value='perigramma'>Περίγραμμα</TabsTrigger>
+				</TabsList>
+				<TabsContent value='rule'>
+					<ThesisRule />
+				</TabsContent>
+				<TabsContent value='perigramma'>
+					<ThesisInfo />
+				</TabsContent>
+			</Tabs>
+		</div>
+	)
+}
+
+export default info
+{
+	/* <div className='w-3/4 m-auto my-4'>
 				<h1 className='text-lg font-semibold my-8'>Περιεχόμενο Διπλωματικής</h1>
 				<p className='text-sm text-gray-600'>
 					Κάθε σπουδαστής, στο πλαίσιο ενός εκ των μαθημάτων του προγράμματος σπουδών, επιλέγει το θέμα στο
@@ -51,9 +83,5 @@ const info = (props: Props) => {
 					with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
 					publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 				</p>
-			</div>
-		</div>
-	)
+			</div> */
 }
-
-export default info
